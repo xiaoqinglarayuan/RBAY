e0bacaffe49bb1de1d5060496b84d9e3088340b0
Lara <xyuan93@gatech.edu>
2026-03-19 12:20:35 -0700

    key generation

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,17 +1,18 @@
 import { client } from '$services/redis';
+import { pageCacheKey } from '$services/keys';
 
 const cacheRoutes = ['/about', '/privacy', '/auth/signin', '/auth/signup'];
 
 export const getCachedPage = (route: string) => {
 	if (cacheRoutes.includes(route)) {
-		return client.get('pagecache#' + route);
+		return client.get(pageCacheKey(route));
 	}
 	return null;
 };
 
 export const setCachedPage = (route: string, page: string) => {
 	if (cacheRoutes.includes(route)) {
-		return client.set('pagecache#' + route, page, {
+		return client.set(pageCacheKey(route), page, {
 			EX: 2
 		});
 	}
