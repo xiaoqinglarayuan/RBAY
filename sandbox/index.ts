9ba154c8dea9a22e3d3b25737a1e7f03080e3fc5
Lara <xyuan93@gatech.edu>
2026-03-19 17:09:20 -0700

    pipeline setup

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,27 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car1', {
+		color: 'red',
+		year: 1950
+	});
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
+	});
+
+	const results = await Promise.all([
+		client.hGetAll('car1'),
+		client.hGetAll('car2'),
+		client.hGetAll('car3')
+	]);
+
+	console.log(results);
+};
+
 run();
