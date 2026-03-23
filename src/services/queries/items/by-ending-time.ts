2df2f17af828d0818f11b00e6e0fbdabda0c1ef9
Lara <xyuan93@gatech.edu>
2026-03-23 13:28:43 -0700

    sorting

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -9,5 +9,7 @@ export const itemsByEndingTime = async (order: 'DESC' | 'ASC' = 'DESC', offset =
 			count
 		}
 	});
-	console.log(ids);
+
+	const results = await Promise.all(ids.map((id) => client.hGetAll(itemsKey(id))));
+	return results.map((item, i) => deserialize(ids[i], item));
 };
