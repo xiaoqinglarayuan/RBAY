e0204c9d842519e9f4d7cd0bb677ff121eeb8a2d
Lara <xyuan93@gatech.edu>
2026-03-23 11:15:19 -0700

    create user

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -2,7 +2,7 @@ import type { CreateItemAttrs } from '$services/types';
 import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { genId } from '$services/utils';
-import { itemsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
 import { deserialize } from './deserialize';
 
 export const getItem = async (id: string) => {
@@ -32,6 +32,13 @@ export const getItems = async (ids: string[]) => {
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId();
 	const serialized = serialize(attrs);
-	await client.hSet(itemsKey(id), serialized);
+
+	await Promise.all([
+		client.hSet(itemsKey(id), serialized),
+		client.zAdd(itemsByViewsKey(), {
+			value: id,
+			score: 0
+		})
+	]);
 	return id;
 };
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,9 +1,22 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey, usernamesUniqueKey } from '$services/keys';
+import { usersKey, usernamesUniqueKey, usernamesKey } from '$services/keys';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+	//Use the username argument to look up the persons User ID
+	// with the usernames sorted set
+	const decimalId = await client.zScore(usernamesKey(), username);
+	//Take the id and convert it back to hex
+	if (!decimalId) {
+		throw new Error('User does not exist');
+	}
+	// Use the id to look up the user's hash
+	const id = decimalId.toString(16);
+	// deserialize and return the hash
+	const user = await client.hGetAll(usersKey(id));
+	return deserialize(id, user);
+};
 
 export const getUserById = async (id: string) => {
 	const user = await client.hGetAll(usersKey(id));
@@ -11,7 +24,7 @@ export const getUserById = async (id: string) => {
 };
 
 export const createUser = async (attrs: CreateUserAttrs) => {
-	const id = genId();
+	const id = genId(); //String
 	// See if the username is already in the set of usernames
 	const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username);
 	//If so, throw an error
@@ -21,7 +34,10 @@ export const createUser = async (attrs: CreateUserAttrs) => {
 	// Otherwise, continue
 	await client.hSet(usersKey(id), serialize(attrs));
 	await client.sAdd(usernamesUniqueKey(), attrs.username);
-
+	await client.zAdd(usernamesKey(), {
+		value: attrs.username,
+		score: parseInt(id, 16)
+	});
 	return id;
 };
 
