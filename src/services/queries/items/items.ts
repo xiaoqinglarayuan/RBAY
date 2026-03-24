1f71060e2e5b6cb6bfdb169ab0567b09cc2dddd2
Lara <xyuan93@gatech.edu>
2026-03-24 11:24:32 -0700

    script

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -2,7 +2,7 @@ import type { CreateItemAttrs } from '$services/types';
 import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { genId } from '$services/utils';
-import { itemsKey, itemsByViewsKey, itemsByEndingAtKey } from '$services/keys';
+import { itemsKey, itemsByPriceKey, itemsByViewsKey, itemsByEndingAtKey } from '$services/keys';
 import { deserialize } from './deserialize';
 
 export const getItem = async (id: string) => {
@@ -42,6 +42,10 @@ export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 		client.zAdd(itemsByEndingAtKey(), {
 			value: id,
 			score: attrs.endingAt.toMillis()
+		}),
+		client.zAdd(itemsByPriceKey(), {
+			value: id,
+			score: 0
 		})
 	]);
 	return id;
--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,11 +1,31 @@
-import { createClient } from 'redis';
+import { createClient, defineScript } from 'redis';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts: {
+		addOneAndStore: defineScript({
+			NUMBER_OF_KEYS: 1,
+			SCRIPT: `
+      return redis.call('SET', KEYS[1], 1+ tonumber(ARGV[1]))
+      `,
+			transformArguments(key: string, value: number) {
+				return [key, value.toString()];
+			},
+			transformReply(reply: any) {
+				return reply;
+			}
+		})
+	}
+});
+
+client.on('connect', async () => {
+	await client.addOneAndStore('book:count', 5);
+	const result = await client.get('book:count');
+	console.log(result);
 });
 
 client.on('error', (err) => console.error(err));
