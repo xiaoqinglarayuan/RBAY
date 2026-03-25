a990d8e9f0f71f63625a7d4e7902b08eb8966c7e
Lara <xyuan93@gatech.edu>
2026-03-25 08:40:03 -0700

    search

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,5 +1,6 @@
 import { createClient, defineScript } from 'redis';
 import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
+import { createIndexes } from './create-indexes';
 
 const client = createClient({
 	socket: {
@@ -59,5 +60,13 @@ const client = createClient({
 
 client.on('error', (err) => console.error(err));
 client.connect();
+
+client.on('connect', async () => {
+	try {
+		await createIndexes();
+	} catch (err) {
+		console.log(err);
+	}
+});
 
 export { client };
